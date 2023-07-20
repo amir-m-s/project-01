b7a2d3a9e63fa559b6b21507cb8d6a7ec5414e19
amir-m-s <71013189+amir-m-s@users.noreply.github.com>
2023-07-20 10:29:33 +0330

    Update project
    Make menu open and close using simple js
    
    Fix menu positions in the next commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let menu_icon = document.querySelector("#menu-icon");
+let menu = document.querySelector(".header > nav > ul");
+
+let is_menu_open = false;
+
+function open_menu(event) {
+    if (!is_menu_open) {
+        menu.style.display = "block";
+        is_menu_open = true;
+    } else {
+        menu.style.display = "none";
+        is_menu_open = false;
+    }
+    console.log("Working!");
+    
+}
+
+menu_icon.addEventListener("click", open_menu);
