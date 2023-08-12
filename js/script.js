e7eb72c843922018bf90805636a90d9e4ec8bed8
amir-m-s <71013189+amir-m-s@users.noreply.github.com>
2023-08-12 16:05:12 +0330

    Update project
    Add nav item change state when scrolling on sections

--- js/script.js
+++ js/script.js
@@ -18,3 +18,53 @@ const go_top = document.querySelector(".go-up");
 go_top.addEventListener("click", () => {
     window.scrollTo({top: 0});
 });
+
+// Nav items change state script
+const sections = [
+    document.querySelector("#home"),
+    document.querySelector("#education"),
+    document.querySelector("#skills"),
+    document.querySelector("#about"),
+    document.querySelector("#contact"),
+]
+
+const li = document.querySelectorAll("nav ul li");
+
+window.addEventListener("scroll", () => {
+    for (let i = 0; i < sections.length; i++) {
+        let section_top = sections[i].getBoundingClientRect().top;
+        let sections_bottom = sections[i].getBoundingClientRect().bottom;
+        
+        if (section_top <= 0 && sections_bottom >= window.innerHeight){
+            // Add active class on relative nav item
+            li[i].classList.add("active");        
+
+            // Remove other nav items active class
+            for (let j=0; j < li.length; j++){
+                if (j!=i) {
+                    li[j].classList.remove("active");
+                }
+            } 
+        }
+    }
+    
+});
+
+window.onscroll = () => {
+    for (let i = 0; i < sections.length; i++) {
+        let section_top = sections[i].getBoundingClientRect().top;
+        let sections_bottom = sections[i].getBoundingClientRect().bottom;
+        
+        if (section_top <= 0 && sections_bottom >= window.innerHeight){
+            // Add active class on relative nav item
+            li[i].classList.add("active");        
+
+            // Remove other nav items active class
+            for (let j=0; j < li.length; j++){
+                if (j!=i) {
+                    li[j].classList.remove("active");
+                }
+            } 
+        }
+    }
+}
